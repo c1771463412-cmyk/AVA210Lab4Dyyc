ec9c4f3c846251435c9c3755119b65a8e0908c8d
Yuyi Chen <c1771463412@gmail.com>
2026-09-05 20:23:08 -0700

    Complete milestone 1

--- lab-4.cpp
+++ lab-4.cpp
@@ -4,3 +4,21 @@
 #include <random>
 using namespace std;
 
+// Define a struct to represent a color with red, green, and blue components
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+
+main() {
+    // Create an instance of the Color struct and initialize it with specific values
+    Color testColor = {255, 155, 55};
+
+    // Output the values of the color components to the console
+    cout << testColor.red << " " 
+         << testColor.green << " " 
+         << testColor.blue << endl;
+
+    return 0;
+}
