bd203d4213c93a611cd5351c59e83f8343aee8f8
Yuyi Chen <c1771463412@gmail.com>
2026-09-05 21:18:54 -0700

    Complete milestone 4

--- lab-4.cpp
+++ lab-4.cpp
@@ -1,10 +1,15 @@
 // COMSC-210-5293 | Lab 4 | Yuyi Chen
 
 #include <iostream>
+
 #include <vector>
+
 #include <cstdlib>
+
 #include <ctime>
+
 #include <iomanip>
+
 using namespace std;
 
 // Define a struct to represent a color with red, green, and blue components
@@ -44,23 +49,23 @@ int main() {
     }
 
     // Print the header for the table
-    cout << right << setw(width) << "Color#"
-         << right << setw(width) << "R Value" 
-         << right << setw(width) << "G Value" 
-         << right << setw(width) << "B Value" << endl;
+    cout << right << setw(width) << "Color#" <<
+        right << setw(width) << "R Value" <<
+        right << setw(width) << "G Value" <<
+        right << setw(width) << "B Value" << endl;
 
     // Print a line of dashes to separate the header from the data
-    cout << right << setw(width) << "------"
-         << right << setw(width) << "-------" 
-         << right << setw(width) << "-------" 
-         << right << setw(width) << "-------" << endl;
+    cout << right << setw(width) << "------" <<
+        right << setw(width) << "-------" <<
+        right << setw(width) << "-------" <<
+        right << setw(width) << "-------" << endl;
 
     // Print the color data in a formatted table
     for (int i = 0; i < colors.size(); i++) {
-        cout << right << setw(width) << i + 1
-             << right << setw(width) << colors[i].red
-             << right << setw(width) << colors[i].green
-             << right << setw(width) << colors[i].blue << endl;
+        cout << right << setw(width) << i + 1 <<
+            right << setw(width) << colors[i].red <<
+            right << setw(width) << colors[i].green <<
+            right << setw(width) << colors[i].blue << endl;
     }
 
     return 0;
